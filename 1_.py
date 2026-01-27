3f49f9f5c03d7f464d21e02ae5cf0989d9ef5fa5
Jy654 <JNavarro69@cps.edu>
2026-01-27 14:35:00 +0000

    fucntions

--- 1_.py
+++ 1_.py
@@ -1,3 +1,57 @@
+# function = a block of reusable code 
+#            place () after the function name to invoke it
+
+# def happy_birthday(name, age):
+#     print(f"Happy birthday to {name}!")
+#     print(f"You are {age} years old!")
+#     print(f"Happy birthday to {name}!")
+#     print()
+
+# happy_birthday("bro", 20) # invoking or calling
+# happy_birthday("Steve", 30)
+# happy_birthday("Joe", 40)
+
+
+# def display_invoice(username, amount, due_date):
+#     print(f"Hello {username}")
+#     print(f"your bill of ${amount:.2f} is due: {due_date}")
+
+# display_invoice("Brocode", 42.50, "01/01")
+
+# return= statement used to end a function 
+#         and send a result back to the cellar
+
+# def add(z,y):
+#     z = x + y
+#     return z
+
+# def subtract(x,y):
+#     z = x - y
+#     return z
+
+# def multiply(x,y):
+#     z = x * y
+#     return z
+
+# def divide(x,y):
+#     z = x / y
+#     return z
+
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(multiply(1, 2))
+# print(divide(1, 2))
+
+
+print(z)
+
+def create_name(first, last):
+    first = first.capatalize()
+    last = last.capatalize()
+    return first + " " + last
+
+full_name = create_name("spongebob")
+
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
 
